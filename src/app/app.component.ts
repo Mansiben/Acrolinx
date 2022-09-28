95db9ffbfb3e102bf3262242eaf240183cb7b3f6
Mansiben Buddhdev <mansithakkar74@gmail.com>
2022-09-27 22:03:04 -0400

    filter box in progress.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,57 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class AppComponent {
 
+  filterValues = {};
   dataSource = new MatTableDataSource();
   displayedColumns: string[] = ['id', 'name', 'username', 'email', 'phone', 'website', 'status'];
 
+  filterSelectObj: any = [];
   constructor(
   ) {
+
+    // Object to create Filter for
+    this.filterSelectObj = [
+      {
+        name: 'ID',
+        columnProp: 'id',
+        options: []
+      }, {
+        name: 'NAME',
+        columnProp: 'name',
+        options: []
+      }, {
+        name: 'USERNAME',
+        columnProp: 'username',
+        options: []
+      }, {
+        name: 'EMAIL',
+        columnProp: 'email',
+        options: []
+      }, {
+        name: 'STATUS',
+        columnProp: 'status',
+        options: []
+      }
+    ]
   }
 
   ngOnInit() {
     this.getRemoteData();
+
+    // Overrride default filter behaviour of Material Datatable
+    this.dataSource.filterPredicate = this.createFilter();
+  }
+
+  // Get Uniqu values from columns to build filter
+  getFilterObject(fullObj, key) {
+    const uniqChk: any = [];
+    fullObj.filter((obj) => {
+      if (!uniqChk.includes(obj[key])) {
+        uniqChk.push(obj[key]);
+      }
+      return obj;
+    });
+    return uniqChk;
   }
 
   // Get remote serve data using HTTP call
@@ -116,6 +158,62 @@ export class AppComponent {
       }
     ];
     this.dataSource.data = remoteDummyData;
+
+    this.filterSelectObj.filter((o) => {
+      o.options = this.getFilterObject(remoteDummyData, o.columnProp);
+    });
   }
 
+  // Called on Filter change
+  filterChange(filter, event) {
+    //let filterValues = {}
+    this.filterValues[filter.columnProp] = event.target.value.trim().toLowerCase()
+    this.dataSource.filter = JSON.stringify(this.filterValues)
+  }
+
+  // Custom filter method fot Angular Material Datatable
+  createFilter() {
+    let filterFunction = function (data: any, filter: string): boolean {
+      let searchTerms = JSON.parse(filter);
+      let isFilterSet = false;
+      for (const col in searchTerms) {
+        if (searchTerms[col].toString() !== '') {
+          isFilterSet = true;
+        } else {
+          delete searchTerms[col];
+        }
+      }
+
+      console.log(searchTerms);
+
+      let nameSearch = () => {
+        let found = false;
+        if (isFilterSet) {
+          for (const col in searchTerms) {
+            searchTerms[col].trim().toLowerCase().split(' ').forEach(word => {
+              if (data[col].toString().toLowerCase().indexOf(word) != -1 && isFilterSet) {
+                found = true
+              }
+            });
+          }
+          return found
+        } else {
+          return true;
+        }
+      }
+      return nameSearch()
+    }
+    return filterFunction
+  }
+
+
+  // Reset table filters
+  resetFilters() {
+    this.filterValues = {}
+    this.filterSelectObj.forEach((value, key) => {
+      value.modelValue = undefined;
+    })
+    this.dataSource.filter = "";
+  }
 }
+
